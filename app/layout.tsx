9a838ed0764760104a410b6c7d2197210923e074
Krish <selfindependentkartik8@gmail.com>
2026-09-08 13:19:36 +0530

    Optimize Google Analytics loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,10 +104,10 @@ export default function RootLayout({
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-BS6TSMM1ZR"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
 
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
